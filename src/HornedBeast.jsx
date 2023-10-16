5ffaa7ff4e7f76b37fcc9f986af2e4ca4badba8f
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-16 11:01:23 -0500

    test commit

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 //from demo code:
-// import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import './App.css'
 // import heart from './images/heart.png'
 //need this?:
@@ -17,32 +17,34 @@ class HornedBeast extends React.Component {
         };
     }
 
-handleLikes = () => {
-    this.setState({
-        likes: this.state.likes + 1,
-        //whatever else needs updating
-    });
-};
+    handleLikes = () => {
+        this.setState({
+            likes: this.state.likes + 1,
+            //whatever else needs updating
+        });
+    };
 
-//pizzaNeeded in demo code here
-//pizzaGot in demo code here
+    //pizzaNeeded in demo code here
+    //pizzaGot in demo code here
 
     //yesterday's stuff:
     render() {
         return (
             <>
-            {/* {name and desc} */}
-            <article>
-                {/* {add props to display} */}
-                <h2>{this.props.title}</h2>
-                {/* <p>{this.state.likes} Likes</p> */}
-                <p className="heart"></p>
-                <p onClick={this.handleLikes}>{'\u2764'} {this.state.likes} Likes </p>
-            </article>
-            
+                {/* {name and desc} */}
+                <article>
+                    {/* {add props to display} */}
+                    <h2>{this.props.title}</h2>
+                    <p onClick={this.handleLikes}>{'\u2764'} {this.state.likes} Likes </p>
+                    <Button variant="primary">Likessss</Button>{' '}
+                </article>
 
 
-                <img src={this.props.imageURL} alt={this.props.description} title={this.props.title}/>
+
+
+
+
+                <img src={this.props.imageURL} alt={this.props.description} title={this.props.title} />
                 <p>{this.props.description}</p>
                 <p>This is a creature.</p>
 
