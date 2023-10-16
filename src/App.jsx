bc41a8279080547b67c39bff9364356db6ce27f4
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-16 01:15:11 -0500

    added App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,40 @@
 
 import React from 'react';
 import '.App.css';
-//import headed component
+import Header from " ./Header.jsx"
+import Footer from "./Footer.jsx"
+import Main from "./MainBody.jsx"
+
+//import header component
 //import the main (html main) componenet
 
-class app extends React.Componenet {
+class App extends React.Componenet {
 
   //can add functions etc
   //need a return method to see stuff in the browser
 
   render() {
-    return {
-  <h1>text</h1>
-  <h2>text</h2>
-  //This is referred to as a magnet.    }
 
+    return (
+    //app component should render a header, footer, and gallery component
+
+    //Header:
+
+    //app component should render a header, footer, and gallery component
+    //Header:
+    <>
+
+    {/*new thingy*/}
+      <Header /> Horned Beasts (but not where to find them)
+      
+      {/* {Main Section Here} */}
+      <Main />
+      
+      <Footer /> by Emma Johnson-Barth
+      
+      </>
+    )
   }
-}
-
-
 }
 
 export default App;
