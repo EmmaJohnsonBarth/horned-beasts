a7f240f6e280680b86a45bc0edb069bd67ceb013
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-16 20:05:27 -0500

    fixed by adding return statement

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -16,13 +16,13 @@ class Gallery extends React.Component {
     //added this.props:
     let beasties = this.props.data.map((newCritter, index) => {
       // beasties.push(
-      <HornedBeast
+      return <HornedBeast
         title={newCritter.title}
         imageURL={newCritter.imageURL}
         addCritter={this.props.addCritter}
         // description={newCritter.description}
         key={index}
-        handleOnShowModal = {this.props.handleOnShowModal}
+        handleOnShowModal={this.props.handleOnShowModal}
       />
     });
 
@@ -33,7 +33,7 @@ class Gallery extends React.Component {
         <main>
           <Container>
             <Row lg={4} md={3} sm={2} xs={1}>
-               {beasties}
+              {beasties}
             </Row>
           </Container>
         </main>
